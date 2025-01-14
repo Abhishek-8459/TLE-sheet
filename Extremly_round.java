9010367ca428d9592e3adb028f3d3038486e4796
Abhishek Gaikwad <abhishekgaikwadlahavit@gmail.com>
2025-01-15 00:26:53 +0530

    add file

---
+++ Extremly_round.java
@@ -0,0 +1,27 @@
+package TLE;
+
+import java.util.*;
+import java.lang.*;
+
+
+public class Extremly_round {
+    public static void main(String[] args) throws java.lang.Exception {
+        // your code goes here
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        for (int y = 0; y < t; y++) {
+            int n = sc.nextInt();
+            String s = Integer.toString(n);
+            String s1= STR."\{s.charAt(0)}";
+            int i = Integer.parseInt(s1);
+            System.out.println(i+(9*(s.length()-1)));
+
+            }
+
+
+
+
+        }
+
+    }
+
